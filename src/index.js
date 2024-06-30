324e7b5e87251079e5d36ddf08e5d081b5d4a1e2
Ashok Choudhary <ashoka8769@gmail.com>
2024-06-30 14:04:53 +0530

    manage hero section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+const isMobile = window.matchMedia("(max-width:1024px)").matches;
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
@@ -12,6 +13,14 @@ initTiltEffect();
 // window.addEventListener("load", () => {
 //   document.getElementById("loading-box").style.display = "none";
 // });
+
+document.getElementById("top").addEventListener("click", () => {
+  document.getElementById("parallax-hero").remove();
+  document.getElementById("hero").style.display = "flex";
+  document.querySelector(".hero-title").style.opacity = "1";
+  document.querySelector(".hero-cta").style.opacity = "1";
+  document.querySelector("#top span").remove();
+});
 
 document.onreadystatechange = function () {
   document.getElementById("loading-box").style.display = "none";
